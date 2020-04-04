80846c282480329416b06e753c8212feb731572f
Christina Lupanow <celupanow@gmail.com>
2020-04-03 22:44:18 -0600

    index.html now has input box and add button, app.js now has functions to render buttons and display gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,28 +2,45 @@
 //create an array called topics to hold the options
 var topics = ["tiramisu", "ice cream", "cake", "eclair", "cupcake", "brownie", "donut", "pudding", "gelato", "pie", "cookies"];
 
-//use a for loop to create and append a button for each topic in the array
+buttons();
 
+//use a for loop to create and append a button for each topic in the array
+function buttons(){
+    $("#buttons").empty();
 for(var i = 0; i < topics.length; i++) {
-    var button = "<button class='dessert'>" + topics[i] + "</button>";
+    var button = $("<button>");
+    button.addClass("dessert");
+    button.attr("data-name", topics[i]);
+    button.text(topics[i]);
     $("#buttons").append(button);
   };
+
+}
 
 //button click function should grab ten static gifs from giphy and display them
 
 function displayGifs() {
 
-    var image = $("<img id='image'>");
-    var item = $(this).attr("item");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hTzhtVnszopo5Jg2X2wKhAc0qcQ6ohGQ&q=" + item + "&limit=1";
+    var item = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hTzhtVnszopo5Jg2X2wKhAc0qcQ6ohGQ&q=" + item + "&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        image.attr("src", response.data[0].source);
-        $("#gifs").append(image);
+        for (var j = 0; j < response.data.length; j++) {
+        var image = $("<img>");
+        image.addClass("image");
+        image.attr("src", response.data[j].images.original_still.url);
+        image.attr("animate", response.data[j].images.original.url);
+        image.attr("still", response.data[j].images.original_still.url);
+        image.attr("data-state", "still");
+        var rating = $("<div>");
+        $("#gifs").prepend(rating);
+        $(rating).prepend("Rating: " + response.data[j].rating);
+        $("#gifs").prepend(image);
         console.log(response);
+        }
 
     });
 }
@@ -31,13 +48,38 @@ function displayGifs() {
 
 $(document).on("click", ".dessert", displayGifs);
 
-
-
-
 //when clicked, gifs should animate, if clicked again, they should stop animating
+
+function gifReaction(){
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("animate"));
+    }
+
+    if (state === "animate") {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("still"));
+    }    
+ 
+}
+
+$(document).on("click", ".image", gifReaction);
 
 //display the rating over every gif
 
 //input box so the user can add something to the array - needs input and button to add
+
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+
+    var gif = $("#gif-input").val().trim();
+
+    topics.push(gif);
+
+    buttons();
+})
 
 //function that runs on the add button click to re create all the buttons
